c30bf297db3dea4aae2366f02d27e2f2eba423da
barthezzko <barthezzko@gmail.com>
2020-01-20 23:37:04 -0500

    initial commit

---
+++ com/kaggle/l1.py
@@ -0,0 +1,28 @@
+spam_amount = 0
+print(spam_amount)
+
+# Ordering Spam, egg, Spam, Spam, bacon and Spam (4 more servings of Spam)
+spam_amount = spam_amount + 4
+
+if spam_amount > 0:
+    print("But I don't want ANY spam!")
+
+viking_song = "Spam " * spam_amount
+print(viking_song)
+
+print(type(spam_amount))
+print(type(12.34))
+print(type(True))
+print(type("Some string here"))
+
+
+print(5 / 2)
+print(6 / 2)
+
+print(5 // 2)
+array = [2, 5, 8, 3]
+print(array)
+print(max(array))
+print(min(array))
+print(abs(-30))
+help(round(-2.01))
